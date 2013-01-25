b5fd5eada09382b3cb2235b1be8ae14ec140c6c4
annelirasu <annelirasu@gmail.com>
2013-01-25 10:15:34 +0200

    Liikide vormil valideerimine töötab, tõlkimata

--- src/p4/controller/AaRapCont.java
+++ src/p4/controller/AaRapCont.java
@@ -5,17 +5,20 @@
 package p4.controller;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import javax.annotation.Resource;
 import org.springframework.context.MessageSource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import p4.dao.AdminYksusDao;
 import p4.domain.AdminYksus;
 
@@ -24,6 +27,7 @@ import p4.domain.AdminYksus;
  * @author reget.kalamees
  */
 @Controller
+@SessionAttributes({"username","admYksused","alluvad","vaYksus"})
 public class AaRapCont {
 
     @Resource
@@ -33,32 +37,50 @@ public class AaRapCont {
 
     //GET päring
     @RequestMapping(value = "/aaRap")
+    @Transactional
     public String ayRap(ModelMap model) {
          Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("username", authentication.getName());
+        //korja adminüksused
         List<AdminYksus> ady = adminYksusDao.getAdmYksAll();
         model.addAttribute("admYksused", ady);
-        
-
+        //võta esimene ja otsi selle alluvad
+        if(ady!=null){
+            //valitud esimene adminüksus
+            
+            AdminYksus vaYksus=ady.get(0);
+            System.out.println("valitud esimene adminüksus " + vaYksus.getNimetus());
+            model.addAttribute("vaYksus", vaYksus);
+           Collection<AdminYksus> alluvad=adminYksusDao.getChilds(vaYksus);
+           
+            String err="OK";
+            if(alluvad==null){err="alluvad null";}
+            model.addAttribute("alluvad", alluvad);
+            model.addAttribute("viiga",err);
+        }
         return "aaRap";
-
     }
 
-    //see saab vormilt andmed
+    //see saab vormilt andmed postiga
     @RequestMapping(value = "/aaRap", method = RequestMethod.POST)
-    public String saveForm(@RequestParam("ayLiik") String ay, @RequestParam("kuupaev") String kp, ModelMap model) {
+    public String saveForm(@RequestParam("aYksus") String ay, ModelMap model) {
 
-
-        System.out.println("Hei! Praegu lisatav on..." + ay + kp);
+      //TODO sisendikontroll
+        
+        System.out.println("Hei! Valiti" + ay);
+        
+        if(ay!=null){
+            long id=Long.parseLong(ay);
+            AdminYksus aYksus=adminYksusDao.getYksusById(id);
+            Collection<AdminYksus> alluvad=adminYksusDao.getChilds(aYksus);
+            model.addAttribute("alluvad", alluvad);
+        }
         return "aaRap";
     }
 
     @RequestMapping(value = "/loginfailed")
     public String loginerror(ModelMap model) {
-
-        //i18 peab ka tehtud olema, siis muutub siin	
-        model.addAttribute("error", "message.loginError");
-
+       model.addAttribute("error", "message.loginError");
         return "login";
     }
 
--- src/p4/dao/AdminYksusDao.java
+++ src/p4/dao/AdminYksusDao.java
@@ -1,5 +1,6 @@
 package p4.dao;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -8,10 +9,14 @@ import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import p4.domain.AdminYksLiik;
 import p4.domain.AdminYksus;
 
 @Repository
+@Transactional 
+@Service
 public class AdminYksusDao {
    @PersistenceContext
     private EntityManager em;
@@ -22,6 +27,35 @@ public class AdminYksusDao {
 	    return q.getResultList(); 
 	     	
 	    }
+          
+          //leiab esimese taseme alluvad
+          @Transactional 
+          public Collection<AdminYksus> getChilds(AdminYksus ay){
+              /*
+             "select riigi_admin_yksus_id,nimetus from RIIGI_ADMIN_YKSUS Where( riigi_admin_yksus_id  in (select alluv_yksus_id  from ADMIN_ALLUVUS where ylemus_yksus_ID=?))";
+              TypedQuery<AdminYksus> query = em.createQuery("select e from ADMINALLUVUS e where e.id = :id",AdminYksus.class);
+                query.setParameter("id", 4L);
+            List<AdminYksus> employees = query.getResultList();
+            */
+              //tiri kõik päringutega mällu
+              System.out.println("enne päringut");
+               TypedQuery<AdminYksus> query = em.createQuery("select a from AdminYksus a", AdminYksus.class);
+               System.out.println("pärast päringut");
+               List<AdminYksus> ayList=query.getResultList();
+                System.out.println("pärast listi");
+              Collection alluvad=ayList.get(0).getAlluvad();
+              if(alluvad==null){System.out.println("Allvaid pole " + ay.getNimetus());}
+              else
+              {System.out.println("Alluvid on " + alluvad.size()+ " ayksusel " + ay.getNimetus());}
+              return alluvad;
+          
+          }
     
+         //anna adminüksus id järgi
+          @Transactional 
+         public AdminYksus getYksusById(Long id){
+          AdminYksus ay = em.find(AdminYksus.class, id);
+          return ay;
+         }
     
 }
